7e5223199742d3d340b3600134ffd5146f7ceaa8
unknown <kraiglyons0929@gmail.com>
2022-09-28 09:27:20 +0800

    added console at starting

--- src/components/dialog-wallet/WalletMixin.vue
+++ src/components/dialog-wallet/WalletMixin.vue
@@ -1,43 +1,44 @@
 <script>
-import Web3 from 'web3'
-import Onboard from 'bnc-onboard'
-import { Network, Wallets, DappId } from '@/config.js'
-import { mapMutations } from 'vuex'
+import Web3 from "web3";
+import Onboard from "bnc-onboard";
+import { Network, Wallets, DappId } from "@/config.js";
+import { mapMutations } from "vuex";
 export default {
   data() {
     return {
-      metaMaskAddress: '',
+      metaMaskAddress: "",
       networkId: null,
-    }
+    };
   },
   watch: {
     metaMaskAddress(cur, old) {
-      this.setMetaMaskAddress(cur)
+      this.setMetaMaskAddress(cur);
     },
     networkId(cur, old) {
       // this.editNet()
-      this.setNetworkId(cur)
+      this.setNetworkId(cur);
     },
   },
   methods: {
-    ...mapMutations(['setMetaMaskAddress', 'setNetworkId', 'setPendings']),
+    ...mapMutations(["setMetaMaskAddress", "setNetworkId", "setPendings"]),
     async editNet() {
-      await Vue.prototype.$onboard.walletCheck()
-      await Vue.prototype.$onboard.getState()
+      await Vue.prototype.$onboard.walletCheck();
+      await Vue.prototype.$onboard.getState();
     },
     async login() {
       try {
-        await Vue.prototype.$onboard.walletSelect()
-        await Vue.prototype.$onboard.walletCheck()
-        await Vue.prototype.$onboard.accountSelect()
+        await Vue.prototype.$onboard.walletSelect();
+        await Vue.prototype.$onboard.walletCheck();
+        await Vue.prototype.$onboard.accountSelect();
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     },
     async onConnect() {
-      const wallets = Wallets
-      const dappId = DappId // [String] The API key created by step one above
-      const networkId = Network // [Integer] The Ethereum network ID your Dapp uses.
+      const wallets = Wallets;
+      const dappId = DappId; // [String] The API key created by step one above
+      const networkId = Network; // [Integer] The Ethereum network ID your Dapp uses.
+      console.log("OnConnect: ", wallets, dappId, networkId);
       Vue.prototype.$onboard = Onboard({
         dappId,
         hideBranding: false,
@@ -47,46 +48,46 @@ export default {
           wallet: (wallet) => {
             Vue.prototype.$web3 = !wallet.provider
               ? null
-              : new Web3(wallet.provider)
-            const walletName = !wallet.name ? 'NoWallet' : wallet.name
-            window.sessionStorage.setItem('EarningSelectedWallet', walletName)
+              : new Web3(wallet.provider);
+            const walletName = !wallet.name ? "NoWallet" : wallet.name;
+            window.sessionStorage.setItem("EarningSelectedWallet", walletName);
           },
           address: (address) => {
             this.metaMaskAddress =
-              typeof address === 'undefined' ? '' : address.toLowerCase()
+              typeof address === "undefined" ? "" : address.toLowerCase();
           },
           network: (network) => {
-            console.log("NETWORK: ", network)
+            console.log("NETWORK: ", network);
             this.networkId =
-              typeof network === 'undefined' ? null : network + ''
+              typeof network === "undefined" ? null : network + "";
           },
           balance: (balance) => {
-            console.log(balance, 'balance')
+            console.log(balance, "balance");
           },
         },
         walletSelect: {
           wallets: wallets,
         },
-      })
+      });
 
-      let previouslySelectedWallet = null
-      const onboard = Vue.prototype.$onboard
+      let previouslySelectedWallet = null;
+      const onboard = Vue.prototype.$onboard;
       if (onboard) {
         previouslySelectedWallet = window.sessionStorage.getItem(
-          'EarningSelectedWallet'
-        )
+          "EarningSelectedWallet"
+        );
         if (
           previouslySelectedWallet &&
-          previouslySelectedWallet !== 'NoWallet'
+          previouslySelectedWallet !== "NoWallet"
         ) {
-          await Vue.prototype.$onboard.walletSelect(previouslySelectedWallet)
-          await Vue.prototype.$onboard.walletCheck()
+          await Vue.prototype.$onboard.walletSelect(previouslySelectedWallet);
+          await Vue.prototype.$onboard.walletCheck();
         }
       }
     },
     async resetApp() {
-      Vue.prototype.$onboard.walletReset()
+      Vue.prototype.$onboard.walletReset();
     },
   },
-}
+};
 </script>
